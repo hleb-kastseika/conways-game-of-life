9397cee3d0e38ae220e2913cfd7bee219223f79a
last-khajiit <last.khajiit@gmail.com>
2015-11-19 14:40:37 +0300

    modify configs

--- conf.lua
+++ conf.lua
@@ -1,8 +1,14 @@
 -- configuration file for LOVE framework
+_CONFIGS = {}
+
 function love.conf(t)
+  t.window.width = 1024
+  t.window.height = 768 
   t.title = "Conway's Game Of Life"
-  t.window.resizable = true
+  t.window.resizable = false
   t.window.fullscreentype = "desktop" 
   t.window.vsync = true
   t.window.icon = "life_icon.png"
+  
+  _CONFIGS = t --save configuration to global variable
 end
